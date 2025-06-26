03b1cc1d212102c6d0b48115f09f8e5fc59e146a
ThieuQuocHuy <fcfvbuhj2001@gmail.com>
2025-06-26 19:04:33 +0700

    feat: Thêm Constant.cs

---
+++ Game_Caro/Constant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Caro
+{
+    public class Constant
+    {
+        // Kích thước ô cờ (pixel)
+        public static int CHESS_WEIGHT = 30;
+        public static int CHESS_HEIGHT = 30;
+        // Kích thước bàn cờ (số dòng và cột)
+        public static int CHESS_BOARD_ROW = 23;
+        public static int CHESS_BOARD_COL = 32;
+        // Cài đặt thời gian chờ mỗi lượt (ms)
+        public static int COOL_DOWN_STEP = 100;
+        public static int COOL_DOWN_TIME = 10000;
+        public static int COOL_DOWN_INTERVAL = 100;
+        public static int COOL_DOWN_TIME1MINUTE = 60000;
+    }
+}
