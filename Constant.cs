83f1d5a2a37c98f835c8ebc84363d58f75301d9e
QHuy156 <huybhq3969@ut.edu.vn>
2025-06-30 01:06:45 +0700

    ADD Constant.cs

---
+++ Constant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Caro
+{
+    public class Constant
+    {
+        public static int CHESS_WEIGHT = 30;
+        public static int CHESS_HEIGHT = 30;
+        public static int CHESS_BOARD_ROW = 23;
+        public static int CHESS_BOARD_COL = 32;
+
+        public static int COOL_DOWN_STEP = 100;
+        public static int COOL_DOWN_TIME = 10000;
+        public static int COOL_DOWN_INTERVAL = 100;
+        public static int COOL_DOWN_TIME1MINUTE = 60000;
+    }
+}
